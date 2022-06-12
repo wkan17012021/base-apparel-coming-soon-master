6f56f7dfd572aca7830436bc1d6497da6c31f8aa
wkan17012021 <wkan@hotmail.co.uk>
2022-06-12 14:39:04 +0100

    layout desktop version draft.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,18 @@ module.exports = {
       mobile: "375px",
       desktop: "1440px",
     },
+    letterSpacing: {
+      custom: "0.5em",
+    },
     extend: {
       colors: {
         desatRed: "hsl(0, 36%, 70%)",
         softRed: "hsl(0, 93%, 68%)",
         dkGrayRed: "hsl(0, 6%, 24%)",
+        offHoverLeft: "hsl(0, 80%, 86%)",
+        offHoverRight: "hsl(0, 74%, 74%)",
+        onHoverLeft: "hsl(0, 60%, 90%)",
+        onHoverRight: "hsl(0, 50%, 80%)",
       },
     },
   },
